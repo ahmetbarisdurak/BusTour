35d50c5d26f10c9afd854ba230bf63d4c7f81cba
ahmet.durak <ahmet.durak@BB11669>
2023-09-12 16:23:47 +0300

    adding new functionalities

--- BusTourMain/BusTour.cpp
+++ BusTourMain/BusTour.cpp
@@ -80,8 +80,7 @@ std::ostream& operator<<(std::ostream& os, Time& time) {
 
 void readInput(StaticVector<Time, TIME_COUNT>& busTimes) {
 
-    std::string input[] = { "10.03", "10.05", "10.08", "10.10", "10.10", "10.15", "10.15", "10.17", "10.20", "10.25", "-1"};
-
+    std::string input[] = { "10.02", "10.02", "10.08", "10.09", "10.13", "10.14", "10.20", "10.20", "10.24", "10.26", "-1" };
     int i = 0;
     while (input[i].compare("-1") != 0 ) {
         Time temp;
@@ -148,180 +147,22 @@ void CreateBusPeriod(StaticVector<Time, TIME_COUNT>& busTimes, int busNumber, Ti
 bool CompareTime(const Time& time1, const Time& time2) {
     return !(time1 < time2);
 }
-/*
-Vector<int, MAX_PULSE> toa;
-Vector<bool, MAX_PULSE> marks;
-
-Vector<Vector<int, MAX_PULSE>, MAX_GROUP> stable_groups;
-Vector<int, MAX_PULSE> stable_periods;
-
-Vector<Vector<int, MAX_GROUP>, MAX_GROUP> staggering_groups;
-Vector<Vector<int, MAX_PULSE>, MAX_GROUP> all_staggering_groups;
 
 
 
-void extract_staggering_groups(Vector<Vector<int, MAX_PULSE>, MAX_GROUP>& groups, Vector<Vector<int, MAX_GROUP>, MAX_GROUP>& groups2) {
-    Vector<int, MAX_GROUP> periods;
-
-    for (int i = 0; i < toa.size(); ++i) {
-        if (marks[i]) continue;
-
-        int maxlen = 0;
-        int period = 0;
-        double maxscore = 0;
-
-        Vector<int, MAX_PULSE> temparr = toa;
-        Vector<int, MAX_PULSE> visited;
-
-        subtract_element(i, temparr);
-
-        if (DEBUG) {
-            std::cout << "Times: " << temparr << std::endl;
-            std::cout << "Checked: " << marks << std::endl;
-        }
-
-        for (int j = i + 3; j < temparr.size(); ++j) {
-            if (marks[j] || toa[j] - toa[i] > MAX_STAGGER_PERIOD) continue;
-
-            int est_period = temparr[j];
-            int prev = temparr[j];
-            int length = 1;
-
-            Vector<int, MAX_PULSE> visited_tmp;
-
-            visited_tmp.push_back(i);
-            visited_tmp.push_back(j);
-
-            for (int k = j + 1; k < temparr.size(); ++k) {
-                if (marks[k]) continue;
-
-                int expected_time = (length + 1) * est_period;
-
-                if (temparr[k] > expected_time) break; //There is no possible value for rest of the array
-                else if (temparr[k] == expected_time) {
-                    length++;
-                    visited_tmp.push_back(k);
-                }
-            }
-
-            est_period = toa[visited_tmp[1]] - toa[visited_tmp[0]];
-
-            for (int k = 0; k < groups.size(); ++k) {
-                if (groups[k].size() == visited_tmp.size() && est_period == periods[k]) {
-                    maxlen = 0;
-                    length = 0;
-                    periods.push_back(est_period);
-                    groups.push_back(visited_tmp);
-                    break;
-                }
-            }
-
-            if (length >= 2 && est_period > period) {
-                period = est_period;
-                visited = visited_tmp;
-                maxlen = length;
-            }
-        }
-
-        if (maxlen != 0) {
-            periods.push_back(period);
-            groups.push_back(visited);
-        }
-    }
-
-    if (PRINT_GROUPS) {
-        std::cout << "STAGGER GROUPS:" << std::endl;
-        for (int i = 0; i < groups.size(); ++i) {
-            std::cout << "index: " << i << " period " << periods[i] << " -- >";
-            for (int j = 0; j < groups[i].size(); ++j) {
-                std::cout << toa[groups[i][j]] << ", ";
-            }
-            std::cout << std::endl;
-        }
-    }
-    std::cout << std::endl;
-
-    Vector<bool, MAX_GROUP> group_marks;
-
-    for (int i = 0; i < groups.size(); ++i) group_marks.push_back(false);
-
-    for (int i = 0; i < groups.size(); ++i) {
-        if (group_marks[i]) continue;
-        Vector<int, MAX_GROUP> visited;
-        visited.push_back(i);
-
-        for (int j = i + 1; j < groups.size(); ++j) {
-            if (groups[i].size() != groups[j].size()) continue;
-
-            if (periods[i] == periods[j])
-                visited.push_back(j);
-        }
-
-        for (int j = 0; j < visited.size(); ++j) group_marks[visited[j]] = true;
-        if (visited.size() >= 3) {
-            groups2.push_back(visited);
-        }
-    }
-
-    for (int i = 0; i < groups.size(); ++i) {
-        if (group_marks[i]) continue;
-        Vector<int, MAX_GROUP> visited;
-        visited.push_back(i);
-
-        for (int j = i + 1; j < groups.size(); ++j) {
-            if (groups[i].size() != groups[j].size()) continue;
-
-            if (periods[i] == periods[j])
-                visited.push_back(j);
-        }
-
-        for (int j = 0; j < visited.size(); ++j) group_marks[visited[j]] = true;
-        if (visited.size() >= 3) {
-            groups2.push_back(visited);
-        }
-    }
-
-
-    /*std::cout << "GROUP COUNT: " << groups2.size() << std::endl;
-    for (int i = 0; i < groups2.size(); ++i) {
-        for (int j = 0; j < groups2[i].size(); ++j) {
-            for (int k = 0; k < groups[groups2[i][j]].size(); ++k) {
-                std::cout << times[groups[groups2[i][j]][i]] << ", ";
-            }
-            std::cout << " | ";
-        }
-
-        std::cout << std::endl;
-    }*/
-/*
-    std::cout << "STAGGER GROUP COUNT: " << groups2.size() << std::endl;
-    for (int i = 0; i < groups2.size(); ++i) {
-        Vector<int, MAX_GROUP>& curgroup = groups2[i];
-        for (int j = 0; j < groups[curgroup[0]].size(); ++j) {
-            for (int k = 0; k < curgroup.size(); ++k) {
-                std::cout << toa[groups[groups2[i][k]][j]] << ", ";
-            }
-            std::cout << " | ";
-        }
-        std::cout << std::endl;
-    }
-
-}
-
-
-
-*/
-
-
-void FindAlternatingPeriod(StaticVector<Time, TIME_COUNT> busTimes, StaticVector<Time, TIME_COUNT> maximumPattern) {
+void FindAlternatingPeriod(StaticVector<Time, TIME_COUNT> busTimes, StaticVector<Time, TIME_COUNT> maximumPattern, StaticVector<Time, TIME_COUNT>& foundPattern) {
     std::cout << "\n-------------------------------------------------" << std::endl;
     std::cout << "Entering the gate with period: " << std::endl;
     std::cout << "Current maximum pattern is " << maximumPattern;
 
     int period1, period2;
     int periods[20][2];
+    bool visited[20][20] = { false };
     int alternatingPeriodCount = 0;
-    std::string count[20] = { " " };
+
+    if (maximumPattern.GetSize() <= 2) // This can't create a alternating pattern
+        return;
+
 
     for (int s = 0; s < maximumPattern.GetSize() - 1; s++) {
         for (int start = 0; start < busTimes.GetSize(); ++start) {
@@ -342,7 +183,7 @@ void FindAlternatingPeriod(StaticVector<Time, TIME_COUNT> busTimes, StaticVector
             for (int index = 0; index < alternatingPeriodCount; ++index) {
                 if (periods[index][0] == period1 && periods[index][1] == period2) {
                     std::cout << "this one already exists " << period1 << "   " << period2 << std::endl;
-                    count[index] += "T";
+                    visited[index][s] = true;
                     found = true;
                 }
             }
@@ -352,27 +193,51 @@ void FindAlternatingPeriod(StaticVector<Time, TIME_COUNT> busTimes, StaticVector
                 std::cout << "this one doesn't exists adding to periods " << period1 << "   " << period2 << std::endl;
                 periods[alternatingPeriodCount][0] = period1;
                 periods[alternatingPeriodCount][1] = period2;
-                count[alternatingPeriodCount] = 1;
+                visited[alternatingPeriodCount][s] = true;
                 alternatingPeriodCount++;
             }
         }
     }
 
     std::cout << "Finding the most occured pattern in the alternatif patterns" << std::endl;
-    int max = 0;
-    int maxIndex = -1;
     std::cout << "Alternatif period count is " << alternatingPeriodCount << std::endl;
+    int foundMaxIndex = 0;
 
     for (int s = 0; s < alternatingPeriodCount; ++s) {
+        foundMaxIndex = 0;
 
-        if (count[s] > max) {
-            max = count[s];
-            maxIndex = s;
+        for (int ss = 0; ss < maximumPattern.GetSize() - 1; ++ss) {
+            if (!visited[s][ss]) {
+                std::cout << periods[s][0] << " and " << periods[s][1] << " not true " << std::endl;
+                foundMaxIndex = -1;
+                break;
+            }
         }
 
+        if (foundMaxIndex == 0)
+            foundMaxIndex = s;
     }
 
-    std::cout << "Maximum one is " << periods[maxIndex][0] << "   " << periods[maxIndex][1] << std::endl;
+    std::cout << "Maximum one is " << periods[foundMaxIndex][0] << "   " << periods[foundMaxIndex][1] << std::endl;
+
+    if (foundMaxIndex == -1)
+        return;
+
+
+    foundPattern.PushBack(maximumPattern[0]);
+    int periodCount = 1;
+    int i = 0;
+    Time nextPeriodPrediction = maximumPattern[0];
+    while(nextPeriodPrediction < maximumPattern[maximumPattern.GetSize() - 1]) {
+
+        nextPeriodPrediction = nextPeriodPrediction + period1;
+        foundPattern.PushBack(nextPeriodPrediction);
+        
+        nextPeriodPrediction = nextPeriodPrediction + period2;
+        foundPattern.PushBack(nextPeriodPrediction);
+        
+    }
+
 }
 
 
@@ -387,6 +252,7 @@ void ExtractAlternatingPeriods(StaticVector<Time, TIME_COUNT> busTimes, bool vis
 
 
         StaticVector<Time, TIME_COUNT> maximumPattern;
+        StaticVector<Time, TIME_COUNT> foundAlternatingPattern;
         int maximumPeriodCount = 0;
 
         for (int j = i + 1; j < busTimes.GetSize(); ++j) {
@@ -420,14 +286,16 @@ void ExtractAlternatingPeriods(StaticVector<Time, TIME_COUNT> busTimes, bool vis
                 }
             }
 
-            if (periodCount >= maximumPeriodCount) {
+            //if (periodCount >= maximumPeriodCount) {
                 maximumPeriodCount = periodCount;
                 maximumPattern = foundPattern;
                 
-                FindAlternatingPeriod(busTimes, maximumPattern);
+                foundAlternatingPattern = StaticVector<Time, TIME_COUNT>();
+
+                FindAlternatingPeriod(busTimes, maximumPattern, foundAlternatingPattern);
                 
-            
-            }
+                std::cout << "Found alternating pattern is " << foundAlternatingPattern;
+            //}
         }
 
 
@@ -436,29 +304,20 @@ void ExtractAlternatingPeriods(StaticVector<Time, TIME_COUNT> busTimes, bool vis
 
         int patternSize = 0;
 
-        for (int k = 0; k < maximumPattern.GetSize(); ++k) {
+        for (int k = 0; k < foundAlternatingPattern.GetSize(); ++k) {
             for (int l = 0; l < busTimes.GetSize(); ++l) {
 
-                if (patternSize == maximumPattern.GetSize())
+                if (patternSize == foundAlternatingPattern.GetSize())
                     break;
 
-                if (maximumPattern[k] == busTimes[l] && !visited[l]) {
+                if (foundAlternatingPattern[k] == busTimes[l] && !visited[l]) {
                     visited[l] = true;
-                    patternSize++;
                     break;
                 }
             }
         }
 
-        if (maximumPattern.GetSize() > 0)
-            std::cout << "Period is: " << maximumPattern[1] - maximumPattern[0] << " Found pattern is " << maximumPattern << std::endl;
     }
-
-
-
-
-
-
 }
 
 
@@ -485,6 +344,8 @@ int main() {
     readInput(busTimes);
 
     ExtractAlternatingPeriods(busTimes, visited);
+
+    std::cout << "Start doing the stable periods" << std::endl;
 
     for (int i = 0; i < busTimes.GetSize(); ++i) {
 
